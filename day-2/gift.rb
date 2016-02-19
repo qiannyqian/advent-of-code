f98d7b88403f5249d735380738525db9ee086ed6
Rui Baltazar <rui@opsmanager.com>
2016-02-19 14:14:08 +0800

    corrected version

--- day-2/gift.rb
+++ day-2/gift.rb
@@ -1,53 +1,39 @@
 class Gift
+  attr_reader :total_paper, :total_ribbon
+  def initialize filename
+    @input_file = filename
+    @total_paper = 0
+    @total_ribbon = 0
+  end
+  
+  def run
+    File.open(@input_file, "r") do |f|
+      f.each_line do |line|
+        l,w,h = parse_line(line)
+        @total_paper += compute_present_paper l, w, h
+        @total_ribbon += compute_ribbon l, w, h
+      end
+    end
+  end
+  
   def parse_line(line)
-    line.strip.split("x").map(&:to_i)
+    line.strip.split("x").map(&:to_i).sort
   end
   
   def compute_present_paper(l,w,h)
-    2 * l * w + 2 * w * h + 2 * h * l
+    total_surface_area = 2 * l * w + 2 * w * h + 2 * h * l
+    total_slack = l*w
+    total_surface_area + total_slack
   end
   
-  def compute_slack(l,w,h)
-    [l * w, w * h, h * l].min
-  end
-  
-  def compute_ribbon_wrap(l,w,h)
-    arr = [l, w, h]
-    arr = arr.sort
-    arr[0] * 2 + arr[1] * 2
-  end
-  
-  def compute_bow(l,w,h)
-    l * w * h
-  end
-  
-  def total_ribbon_length
-    total_ribbon = 0
-    
-    File.open("input", "r") do |f|
-      f.each_line do |line|
-        l,w,h = parse_line(line)
-        ribbon_wrap = compute_ribbon_wrap(l,w,h)
-        bow = compute_bow(l,w,h)
-        total_ribbon += ribbon_wrap + bow
-      end
-    end
-    
-    total_ribbon
-  end
-  
-  def total_wrap_paper
-    total_paper = 0
-
-    File.open("input", "r") do |f|
-      f.each_line do |line|
-        l,w,h = parse_line(line)
-        present_paper = compute_present_paper(l,w,h)
-        small_face = compute_slack(l,w,h)
-        total_paper += present_paper + small_face
-      end
-    end
-    
-    total_paper
+  def compute_ribbon(l, w, h)
+    ribbon_length = 2*l + 2*w
+    bow_length = l * w * h
+    ribbon_length + bow_length
   end
 end
+
+
+#gift = Gift.new 'input'
+#gift.run
+#puts "#{gift.total_paper}, #{gift.total_ribbon}"
--- day-2/gift_test.rb
+++ day-2/gift_test.rb
@@ -3,53 +3,46 @@ require_relative "./gift"
 
 describe Gift do
   before do
-    @gift = Gift.new
+    @gift = Gift.new 'input'
   end
-  
-  describe 'load correct values from string' do
-    it 'sets w,l,h correctly' do
-      @gift.parse_line('21x23x49').must_equal [21, 23, 49]
+
+  describe 'unit test' do
+    describe '#parse_line' do
+      it 'sets w,l,h correctly' do
+        @gift.parse_line('49x21x23').must_equal [21, 23, 49]
+      end
     end
-  end
-  
-  describe 'compute the present paper' do
-    it 'calculates according to described function' do
-      @gift.compute_present_paper(2, 3, 4).must_equal 52
-      @gift.compute_present_paper(1, 1, 10).must_equal 42
+    
+    describe 'compute the present paper' do
+      it 'calculates according to described function' do
+        @gift.compute_present_paper(2, 3, 4).must_equal 58
+        @gift.compute_present_paper(1, 1, 10).must_equal 43
+      end
     end
-  end
-  
-  describe 'slack calculation' do
-    it 'calculates the slack based on smallest face' do
-      @gift.compute_slack(2, 3, 4).must_equal 6
-      @gift.compute_slack(1,1,10).must_equal 1
-    end
-  end
-  
-  describe 'total amount of wrapping paper' do
-    it 'sums all the amount based on the input' do
-      @gift.total_wrap_paper.must_equal 1606483
-    end
-  end
-  
-  describe 'length for wrapping ribbon' do
-    it 'calculates the length required for wrapping' do
-      @gift.compute_ribbon_wrap(2,3,4).must_equal 10
-      @gift.compute_ribbon_wrap(1,1,10).must_equal 4
-    end
-  end
-  
-  describe 'length for bow' do
-    it 'calculates the length required for bow' do
-      @gift.compute_bow(2,3,4).must_equal 24
-      @gift.compute_bow(1,1,10).must_equal 10
-    end
-  end
-  
-  describe 'total ribbon length' do
-    it 'calculates the sum of wrapping ribbon and bow length' do
-      @gift.total_ribbon_length.must_equal 3842356
+    
+    describe 'length for wrapping ribbon' do
+      it 'calculates the length required for wrapping' do
+        @gift.compute_ribbon(2,3,4).must_equal 34
+        @gift.compute_ribbon(1,1,10).must_equal 14
+      end
     end
   end
 
+  describe 'run with input' do
+    before do
+      @gift.run
+    end
+    
+    describe 'total amount of wrapping paper' do
+      it 'sums all the amount based on the input' do
+        @gift.total_paper.must_equal 1606483
+      end
+    end
+    
+    describe 'total ribbon length' do
+      it 'calculates the sum of wrapping ribbon and bow length' do
+        @gift.total_ribbon.must_equal 3842356
+      end
+    end
+  end
 end
